1ea41d8e158b5f52324c91af0c5c8605de688770
Gabor Janvari (DELL) <janvari.gabor2@gmail.com>
2018-10-25 16:29:55 +0200

    Próbálkozás

--- src/AdatBekeroAblak.java
+++ src/AdatBekeroAblak.java
@@ -1,35 +1,56 @@
-
+import java.awt.Component;
 import javax.swing.*;
-import java.awt.*;
+import static javax.swing.GroupLayout.Alignment.*;
+ 
+public class AdatBekeroAblak extends JFrame {
+    public AdatBekeroAblak() {
+        JLabel label = new JLabel("Név:");;
+        JTextField textField = new JTextField(40);
+       
+        JButton findButton = new JButton("Ok");
+        JButton cancelButton = new JButton("Mégsem");
+ 
+        GroupLayout layout = new GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setAutoCreateGaps(true);
+        layout.setAutoCreateContainerGaps(true);
+ 
+        layout.setHorizontalGroup(layout.createSequentialGroup()
+            .addComponent(label)
+            .addComponent(textField)
+            .addComponent(findButton)
+           .addComponent(cancelButton)
+        );
+        
 
-public class AdatBekeroAblak {
-   JFrame keret;
-   public static void main(String[] args) {
-      AdatBekeroAblak ablak = new AdatBekeroAblak();
-      ablak.Bekeres();
-   }
-
-   public void AblakElemek() {
-      keret = new JFrame("Személyi adatok bekérése");
-      keret.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      keret.setSize(600, 400);
-      keret.setResizable(false);
-      
-      JPanel panelf = new JPanel();
-      SpringLayout layout = new SpringLayout();
-      JPanel panela = new JPanel();
-      panela.setBackground(Color.DARK_GRAY);
-      panelf.setBackground(Color.ORANGE);
-      keret.getContentPane().add(BorderLayout.SOUTH, panela);
-      keret.getContentPane().add(BorderLayout.NORTH, panelf);
-      keret.setVisible(true);
-      
-      
-   }
-      
-   public void Bekeres() {
-      AblakElemek();
-   }
+ 
+        layout.setVerticalGroup(layout.createParallelGroup(CENTER)
+                .addComponent(label)
+                .addComponent(textField)
+                .addComponent(findButton)
+                .addComponent(cancelButton));
+        
+ 
+        setTitle("Személyes adatok bekérése");
+        pack();
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+     
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    UIManager.setLookAndFeel(
+                                  "javax.swing.plaf.metal.MetalLookAndFeel");
+                                //  "com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+                                //UIManager.getCrossPlatformLookAndFeelClassName());
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+                new AdatBekeroAblak().setVisible(true);
+            }
+        });
+    }
 }
 
 
